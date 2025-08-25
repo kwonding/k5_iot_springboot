0f1a816c8ebd7315f7d1b125a6a590e4585c5cf8
kwonding <wldo8541@naver.com>
2025-08-25 16:43:25 +0900

    20250825 JWT Authentication revised

--- src/main/java/com/example/k5_iot_springboot/config/WebSecurityConfig.java
+++ src/main/java/com/example/k5_iot_springboot/config/WebSecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -112,18 +111,18 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(
-                                "api/v1/auth/**"
+                                "/api/v1/auth/**"
                         ).permitAll()
                         // 읽기 공개 예시 (게시글 목록, 조회 등)
-                        .requestMatchers(HttpMethod.GET, "api/v1/boards/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/boards/**").permitAll()
                         .anyRequest().authenticated() // 나머지는 인증 필요
                 );
-        if (h2ConsoleEnabled) {
-            http.headers(headers
-                    -> headers.frameOptions(frame -> frame.sameOrigin()));
-            http.authorizeHttpRequests(auth
-                    -> auth.requestMatchers("/h2-console/**").permitAll());
-        }
+//        if (h2ConsoleEnabled) {
+//            http.headers(headers
+//                    -> headers.frameOptions(frame -> frame.sameOrigin()));
+//            http.authorizeHttpRequests(auth
+//                    -> auth.requestMatchers("/h2-console/**").permitAll());
+//        }
 
         // JWT 인증 필터를 UsernamePasswordAuthenticationFilter 앞에 배치
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
